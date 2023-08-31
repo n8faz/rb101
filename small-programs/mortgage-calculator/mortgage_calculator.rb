e26a15c57adf3007af93aee027a0531a79ba882f
n8faz <119473201+n8faz@users.noreply.github.com>
2023-08-31 15:32:13 -0700

    Attempt to fix syntax highlighting github 2

--- small-programs/mortgage-calculator/mortgage_calculator.rb
+++ small-programs/mortgage-calculator/mortgage_calculator.rb
@@ -131,10 +131,7 @@ def calculate_monthly_payment(loan_amount, monthly_interest, duration_in_months)
   if monthly_interest == 0
     monthly_payment = loan_amount / duration_in_months
   else
-    monthly_payment = (loan_amount *
-                      (monthly_interest /
-                      (1 -
-                      ((1 + monthly_interest)**(-duration_in_months)))))
+    monthly_payment = loan_amount * (monthly_interest / (1 -((1 + monthly_interest)**(-duration_in_months))))
   end
   monthly_payment.round(2)
 end
