46acfcceb6e5594383be211f69c54e63f0363699
n8faz <119473201+n8faz@users.noreply.github.com>
2023-09-01 15:47:33 -0700

    Complete Easy 1 Question 8

---
+++ practice-problems/easy-1/question_8.rb
@@ -0,0 +1,9 @@
+# Given the Hash
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+# Create an array containing only two elements: Barney's name and Barney's number
+
+barney_array = flintstones.assoc("Barney")
+
+p barney_array
