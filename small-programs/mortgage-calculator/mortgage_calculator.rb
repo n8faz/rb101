73d9478f6f395076934858672389b2259d32cd88
n8faz <119473201+n8faz@users.noreply.github.com>
2023-08-31 18:46:15 -0700

    Define snippet language as Ruby not YAML

--- small-programs/mortgage-calculator/mortgage_calculator.rb
+++ small-programs/mortgage-calculator/mortgage_calculator.rb
@@ -133,7 +133,7 @@ def calculate_monthly_payment(loan_amount,
     monthly_payment = loan_amount / duration_in_months
   else
     monthly_payment = loan_amount *
-                      (monthly_interest /
+                      (monthly_interest / 
                       (1 - ((1 + monthly_interest)**(-duration_in_months))))
   end
   monthly_payment.round(2)
