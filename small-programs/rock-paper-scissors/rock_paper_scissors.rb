f973aadade1d99e794557b1ecb962665cdb65cd3
n8faz <119473201+n8faz@users.noreply.github.com>
2023-09-06 17:13:45 -0700

    Refactor after code review

--- small-programs/rock-paper-scissors/rock_paper_scissors.rb
+++ small-programs/rock-paper-scissors/rock_paper_scissors.rb
@@ -8,6 +8,8 @@ MOVES = { 'rock' => { abbrev: 'r', beats: ['scissors', 'lizard'] },
           'spock' => { abbrev: 'sp', beats: ['scissors', 'rock'] } }
 ROUNDS_TO_WIN = 3
 
+# Methods that return
+
 def clear_screen
   system "clear"
 end
@@ -18,10 +20,6 @@ end
 
 def arrow_prompt(message)
   puts "=> #{message}"
-end
-
-def no_arrow_prompt(message)
-  puts message
 end
 
 def valid_abbreviation
@@ -36,11 +34,36 @@ def convert_abbreviation(choice)
   (MOVES.map { |k, v| k if v[:abbrev] == choice }).join
 end
 
+def win?(first, second)
+  MOVES.dig(first, :beats).include?(second)
+end
+
+def get_computer_choice
+  MOVES.keys.sample
+end
+
+def get_player_choice
+  choice = nil
+  loop do
+    arrow_prompt(messages('choose'))
+    choice = gets.chomp.downcase
+    if valid_abbreviation.include?(choice)
+      choice = convert_abbreviation(choice)
+      break
+    elsif valid_choice?(choice)
+      break
+    else
+      arrow_prompt(messages('invalid_choice'))
+    end
+  end
+  choice
+end
+
 def get_name
   name = nil
   loop do
     name = gets.chomp
-    if name.empty? || name.start_with?(' ')
+    if name.empty? || name.start_with?
       arrow_prompt(messages('invalid_name'))
     else
       break
@@ -67,6 +90,74 @@ def play?
   answer
 end
 
+def rematch
+  arrow_prompt(messages('rematch'))
+  sleep 2
+  'rematch'
+end
+
+def play_again?
+  answer = nil
+  loop do
+    arrow_prompt(messages('again?'))
+    answer = gets.chomp
+    if answer.downcase.start_with?('y')
+      answer = rematch
+      break
+    elsif answer.downcase.start_with?('n')
+      answer = 'no'
+      break
+    else
+      arrow_prompt(messages('again_invalid'))
+    end
+  end
+  answer
+end
+
+def next_round?
+  answer = nil
+  arrow_prompt(messages('next_round?'))
+  loop do
+    answer = gets.chomp
+    if answer.downcase.start_with?('y')
+      break
+    elsif answer.downcase.start_with?('q')
+      answer = 'quit'
+      arrow_prompt(messages('quitting'))
+      break
+    else
+      arrow_prompt(messages('wait'))
+    end
+  end
+  answer
+end
+
+def game_over?(score)
+  (score[:player] || score[:computer]) == ROUNDS_TO_WIN
+end
+
+def keep_score(choices, score)
+  if win?(choices[:player], choices[:computer])
+    score[:player] += 1
+  elsif win?(choices[:computer], choices[:player])
+    score[:computer] += 1
+  end
+  score
+end
+
+# Methods that print
+
+def print_intro(name)
+  arrow_prompt("Hi, #{name}!")
+  puts
+  arrow_prompt(messages('rules'))
+  puts
+  arrow_prompt(messages('rounds') + "#{ROUNDS_TO_WIN} rounds is the Champion!")
+  puts
+  arrow_prompt(messages('opponent'))
+  puts
+end
+
 def print_which_exit_message?(play, name)
   if play == 'no'
     arrow_prompt(messages('didnt_play') + "#{name}.")
@@ -85,54 +176,26 @@ def print_round(round)
   arrow_prompt(messages('line') + " Round #{round} " + messages('line'))
 end
 
-def print_current_score(name, score)
-  arrow_prompt(messages('current_score'))
-  arrow_prompt("#{name}: " + score[:player].to_s)
-  arrow_prompt(messages('computer_score') + score[:computer].to_s)
-end
-
-def print_final_score(name, score)
-  no_arrow_prompt(' ')
-  arrow_prompt(messages('final_score'))
-  arrow_prompt("#{name}: " + score[:player].to_s)
-  arrow_prompt(messages('computer_score') + score[:computer].to_s)
-end
-
-def get_player_choice
-  choice = nil
-  loop do
-    arrow_prompt(messages('choose'))
-    choice = gets.chomp.downcase
-    if valid_abbreviation.include?(choice)
-      choice = convert_abbreviation(choice)
-      break
-    elsif valid_choice?(choice)
-      break
-    else
-      arrow_prompt(messages('invalid_choice'))
-    end
+def print_score(name, score)
+  if game_over?(score) == true
+    arrow_prompt(messages('final_score'))
+  else
+    arrow_prompt(messages('current_score'))
   end
-  choice
-end
-
-def get_computer_choice
-  MOVES.keys.sample
+  arrow_prompt("#{name}: " + score[:player].to_s)
+  arrow_prompt(messages('computer_score') + score[:computer].to_s)
 end
 
 def print_thinking
   arrow_prompt(messages('thinking'))
   sleep 2
-  no_arrow_prompt(' ')
+  puts
 end
 
 def print_choices(choices)
   arrow_prompt(messages('you_chose') + choices[:player].to_s.capitalize)
   arrow_prompt(messages('computer_chose') + choices[:computer].to_s.capitalize)
-  no_arrow_prompt(' ')
-end
-
-def win?(first, second)
-  MOVES.dig(first, :beats).include?(second)
+  puts
 end
 
 def print_player_beats_computer(choices)
@@ -155,57 +218,27 @@ def print_results(choices)
   else
     arrow_prompt(messages('tie'))
   end
-  no_arrow_prompt(' ')
+  puts
 end
 
-def keep_score(choices, score)
-  if win?(choices[:player], choices[:computer])
-    score[:player] += 1
-  elsif win?(choices[:computer], choices[:player])
-    score[:computer] += 1
-  end
-  score
-end
-
-def next_round?
-  answer = nil
-  arrow_prompt(messages('next_round?'))
-  loop do
-    answer = gets.chomp
-    if answer.downcase.start_with?('y')
-      break
-    elsif answer.downcase.start_with?('q')
-      answer = 'quit'
-      arrow_prompt(messages('quitting'))
-      break
-    else
-      arrow_prompt(messages('wait'))
-    end
-  end
-  answer
-end
-
-def game_over?(name, score)
+def print_champion(name, score)
   if score[:player] == ROUNDS_TO_WIN
     arrow_prompt(messages('player_champion'))
-    print_final_score(name, score)
-    true
+    puts
+    print_score(name, score)
+
   elsif score[:computer] == ROUNDS_TO_WIN
     arrow_prompt(messages('computer_champion'))
-    print_final_score(name, score)
-    true
-  else
-    false
+    puts
+    print_score(name, score)
   end
 end
 
-def round_loop(name,
-               score,
-               current_round)
+def round_loop(name, score, current_round)
   loop do
     current_round += 1
     print_round(current_round)
-    print_current_score(name, score)
+    print_score(name, score)
 
     choices = { player: get_player_choice,
                 computer: get_computer_choice }
@@ -215,32 +248,10 @@ def round_loop(name,
     print_results(choices)
 
     score = keep_score(choices, score)
-    break if game_over?(name, score) || next_round? == 'quit'
-  end
-end
 
-def rematch
-  arrow_prompt(messages('rematch'))
-  sleep 2
-  'yes'
-end
-
-def play_again?
-  answer = nil
-  loop do
-    arrow_prompt(messages('again?'))
-    answer = gets.chomp
-    if answer.downcase.start_with?('y')
-      answer = rematch
-      break
-    elsif answer.downcase.start_with?('n')
-      answer = 'no'
-      break
-    else
-      arrow_prompt(messages('again_invalid'))
-    end
+    print_champion(name, score)
+    break if game_over?(score) || next_round? == 'quit'
   end
-  answer
 end
 
 # Program Start
@@ -249,49 +260,18 @@ clear_screen
 
 arrow_prompt(messages('welcome'))
 name = get_name
+print_intro(name)
 
-info = <<-MSG
-Hi, #{name}!
-
-This game varies a bit from the traditional Rock, Paper, Scissors...
-
-In the traditional game:
-
-Rock crushes Scissors,
-Scissors cuts Paper,
-and Paper covers Rock.
-
-In this game, you can also choose Lizard or Spock:
-
-Rock crushes Lizard,
-Lizard poisons Spock,
-Spock smashes Scissors,
-Scissors decapitates Lizard,
-Lizard eats Paper,
-Paper disproves Spock,
-and Spock vaporizes Rock.
-
-The game will consist of a series of rounds. First to win 3 rounds is the Champion!
-
-I, the Computer, will be your opponent. Don't worry, I won't cheat!
-
-MSG
-
-arrow_prompt(info)
 play = play?
-
 if play == 'yes'
   print_start
-
   loop do # game loop
     score = { player: 0,
               computer: 0 }
     current_round = 0
-    round_loop(name,
-               score,
-               current_round)
-    no_arrow_prompt(' ')
-    break unless play_again? == 'yes'
+    round_loop(name, score, current_round)
+    puts
+    break unless play_again? == 'rematch'
   end
 end
 
