74a46bc1422a9964eeedc96fd96848d282fb6c45
n8faz <119473201+n8faz@users.noreply.github.com>
2023-09-20 18:49:40 -0700

    Complete Medium 1 Question 9

---
+++ practice-problems/medium-1/question_9.rb
@@ -0,0 +1,18 @@
+# Consider these two simple methods:
+
+def foo(param = "no")
+  "yes"
+end
+
+def bar(param = "no")
+  param == "no" ? "yes" : "no"
+end
+
+# What would be the return value of the following method invocation?
+
+p bar(foo)
+
+# no 
+
+# foo == "yes" so bar returns no
+
