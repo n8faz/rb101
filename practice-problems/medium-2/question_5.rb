4d0544c98a5cc979565f58ca1ef42f17507b16f4
n8faz <119473201+n8faz@users.noreply.github.com>
2023-09-21 12:53:38 -0700

    Complete Medium 2 Question 5

--- practice-problems/medium-2/question_5.rb
+++ practice-problems/medium-2/question_5.rb
@@ -2,12 +2,13 @@
 
 def tricky_method(a_string_param, an_array_param)
   a_string_param += "rutabaga"
-  an_array_param << "rutabaga"
+  an_array_param += ["rutabaga"]
+  return a_string_param, an_array_param
 end
 
 my_string = "pumpkins"
 my_array = ["pumpkins"]
-tricky_method(my_string, my_array)
+my_string, my_array = tricky_method(my_string, my_array)
 
 puts "My string looks like this now: #{my_string}"
 puts "My array looks like this now: #{my_array}"
@@ -15,3 +16,5 @@ puts "My array looks like this now: #{my_array}"
 # Whether the above "coincidentally" does what we think we wanted depends upon what is going on inside the method.
 
 # How can we change this code to make the result easier to predict and easier for the next programmer to maintain? That is, the resulting method should not mutate either argument, but my_string should be set to 'pumpkinsrutabaga' and my_array should be set to ['pumpkins', 'rutabaga']
+
+# assign return values of the method to the set local variables
