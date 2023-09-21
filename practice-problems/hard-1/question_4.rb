e2496e9b9417f0c436cc72adcf71953671580303
n8faz <119473201+n8faz@users.noreply.github.com>
2023-09-21 14:47:02 -0700

    Complete Hard 1 Question 4

--- practice-problems/hard-1/question_4.rb
+++ practice-problems/hard-1/question_4.rb
@@ -2,13 +2,20 @@
 
 def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split(".")
+  return false if dot_separated_words.size != 4
   while dot_separated_words.size > 0 do
     word = dot_separated_words.pop
-    break unless is_an_ip_number?(word)
+    return false unless is_an_ip_number?(word)
   end
-  return true
+  true
 end
 
 # Alyssa reviewed Ben's code and says "It's a good start, but you missed a few things. You're not returning a false condition, and you're not handling the case that there are more or fewer than 4 components to the IP address (e.g. "4.5.5" or "1.2.3.4.5" should be invalid.)"
 
 # Help Ben fix his code 
+
+def is_an_ip_number?(word)
+  0 < word.to_i && word.to_i < 255
+end
+
+p dot_separated_ip_address?("4.5.5")
